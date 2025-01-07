4e7ae1c34dedbf3674b45311ff535d8e6d0b62a5
ANKIT KUMAR DUBEY <ankit.krdubey07@gmail.com>
2025-01-07 12:05:51 +0530

    Add files via upload

---
+++ BACKEND/Mongo/2_FIND_IN_MONGOOSE/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String,
+});
+const User = mongoose.model("User", userSchema);
+User.find({age:{$gt:22}}).then((res)=>{
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+});
